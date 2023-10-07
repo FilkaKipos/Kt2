d40b44f43d7ea5bcc018cfddecdf7ea6094efd11
GhostBang <rezidreta@gmail.com>
2023-10-07 13:15:00 +0500

    first commit

---
+++ bin/kt_2.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+
+import 'models/cars/cars.dart';
+import 'models/getprod/getprod.dart';
+void main(List<String> arguments) async{
+  Dio client = Dio();
+  String url = 'https://myfakeapi.com/api/cars/';
+  Response response = await client.get(url);
+
+  GetProd getprod = GetProd.fromJson(response.data);
+  List<Car> carM = getprod.cars;
+
+  double srprice=0;
+  int count=0;
+  for (var el in carM) {
+    if(el.car_color=='Yellow'){
+      String pr = el.price.substring(1);
+      double prd = double.parse(pr);
+      srprice+=prd;
+      count++;
+      
+    }
+    
+  }
+  srprice/=count;
+  print(srprice.toStringAsFixed(3));
+}
